8b62f66eabb8c5fc1aebd0cf18b59398bfc43a2e
snayeemu <snayeemu2@gmail.com>
2023-05-06 15:38:55 +0600

    updated

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-app.use(cors( ));
+app.use(cors());
 
 const categories = require("./data/categories.json");
+
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("Dragon is running");
@@ -13,6 +15,26 @@ app.get("/", (req, res) => {
 
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/categories/:categoryId", (req, res) => {
+  const id = req.params.categoryId;
+  if (id + ""  === "0") res.send(news);
+  else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    console.log(categoryNews);
+    res.send(categoryNews);
+  }
 });
 
 app.listen(port, () => {
